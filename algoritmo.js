82a94ed8d5d4a0563927e640f9fc6aef36dbc8f0
Johanna Riquelme <riquelme.johanna07@gmail.com>
2023-03-31 11:56:40 -0300

    Segunda Parte Práctica 1.6

--- algoritmo.js
+++ algoritmo.js
@@ -1,5 +1,19 @@
-let colorfavorito = prompt ("Cuál es tu color favorito");
+let numeroEntero = prompt ("Ingresar el tipo de dato integer/entero");
+let booleano = prompt ("Ingresar el tipo de dato booleano");
+let numeroDecimal = prompt ("Ingresar un número decimal");
+let caracter = prompt ("Ingresar un caracter");
+let palabra = prompt ("Ingresar una palabra");
+
+document.write ("<h1> Entero:"+ numeroEntero + "</H1>");
+document.write ("<h2> Booleano:"+ booleano + "</H2>");
+document.write ("<h3> Decimal:"+ numeroDecimal + "</H3>");
+document.write ("<h4> caracter:"+ caracter + "</H4>");
+document.write ("<h5> Palabra (conjunto de carácteres): "+ palabra + "</H5>");
+
+
+
+/* let colorfavorito = prompt ("Cuál es tu color favorito");
 
 console.log (colorfavorito);
 document.querySelector("body").innerHTML= "<h1>"+colorfavorito+"</h1>"
-document.write ("<h2>"+colorfavorito+"</h2>");
+document.write ("<h2>"+colorfavorito+"</h2>"); */
